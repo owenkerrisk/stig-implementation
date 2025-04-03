3288d303d3fa55da58001529b19233b312c4c3fd
Owen Kerrisk <okerrisk@hotmail.com>
2025-04-03 17:01:45 +0100

    Update WN10-AU-000500.ps1

--- STIGS/WN10-AU-000500.ps1
+++ STIGS/WN10-AU-000500.ps1
@@ -4,9 +4,9 @@
     This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
 
 .NOTES
-    Author          : Trevino Parker
-    LinkedIn        : linkedin.com/in/trevinoparker/
-    GitHub          : github.com/trevinoparker7
+    Author          : Owen Kerrisk
+    LinkedIn        : linkedin.com/in/owen-kerrisk-b7743085/ 
+    GitHub          : https://github.com/owenkerrisk
     Date Created    : 2024-09-09
     Last Modified   : 2024-09-09
     Version         : 1.0
